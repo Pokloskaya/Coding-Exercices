c0e374373354f87572a2083f2ef5945765fccf15
Julian Romero <master7n7@gmail.com>
2022-12-06 23:57:16 -0500

    added math exercise

--- cp/1A(REVISAR).cpp
+++ cp/1A(REVISAR).cpp
@@ -1,31 +1,63 @@
+// https://codeforces.com/problemset/problem/1/A
+
 #include <iostream>
 #include <bits/stdc++.h>
- 
 using namespace std;
  
 int main() {
  
-  long long int n,m,a;
+  double n,m,a, resultado;
   cin >> n;
   cin >> m;
   cin >> a;
- 
-  long long int contador = a;
-  long long int aa = a;
- 
-  long long int i;
-  for(i=1; n>a; i++){
-    a = a+contador;
+
+  double divisionN; double divisionM;
+  divisionN = n/a;
+  divisionM = m/a;
+
+  if(n + a == 2){
+    cout << m;
+    exit(0);
   }
-  i = i;
-  //cout <<i<<endl;
  
-  long long int ii;
-  for(ii=1; m>aa; ii++){
-    aa = aa+contador;
+  if(m + a == 2){
+    cout << n;
+    exit(0);
   }
   
-  ii = ii;
-  //cout <<ii<<endl;
-  cout<<i+ii;
+  if((n == a) && ()){
+    
+  }
+  //si n, m es igual a 'a'
+  if(n && m == a){
+    cout << 1;
+    exit(0);
+  }
+  
+  //----------con enteros 
+  int nEntero = int(n);
+  int mEntero = int(m);
+  int aEntero = int(a);
+  int divisionNentero; int divisionMentero;
+
+  divisionNentero = nEntero/aEntero;
+  divisionMentero = mEntero/aEntero;
+
+  double divisionNaDouble = divisionNentero;
+  double divisionMaDouble = divisionMentero;
+  //---------------------
+
+  // resultado = divisionN+divisionM;
+  // cout << resultado;  divisionN
+  //cout << divisionN;
+  if(divisionN > divisionNaDouble){
+    divisionNentero++;
+  }
+
+  if(divisionM > divisionMaDouble){
+    divisionMentero++;
+  }
+
+  resultado = divisionNentero+divisionMentero;
+  cout << resultado;
 }
