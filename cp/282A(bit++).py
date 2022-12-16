6926a11d33fe274238ec28d19788a19709846540
Julian Romero <master7n7@gmail.com>
2022-12-15 23:44:17 -0500

    added simple exercise

---
+++ cp/282A(bit++).py
@@ -0,0 +1,12 @@
+numberOperations = input()
+numberOperations = (int(numberOperations))
+
+contador = 0
+for i in range(numberOperations):
+    operation = input()
+    if(operation[1] == "+"):
+        contador = contador+1
+    else:
+        contador = contador-1
+
+print(contador)
