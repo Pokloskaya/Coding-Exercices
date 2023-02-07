fc128e63dd63aa8079b43d8b5c3bdcb6d5acc709
Pokloskaya <master7n7@gmail.com>
2023-02-07 17:12:36 -0500

    Done simple string exercise and added an algorithm to find the prime factors of a given number

---
+++ cp/fubol.py
@@ -0,0 +1,35 @@
+#https://codeforces.com/problemset/problem/96/A
+n = input()
+flag = False
+for i in range(len(n)):
+    if(i + 6 < len(n)):
+        if(n[i] == n[i+1] == n[i+2] == n[i+3] == n[i+4] == n[i+5] == n[i+6]):
+            flag = True
+
+if(flag):
+    print("YES")
+else:
+    print("NO")
+
+
+# //https://codeforces.com/problemset/problem/96/A
+# #include <bits/stdc++.h>
+# using namespace std;
+
+# int main(){
+
+#     string n; cin >> n;
+#     bool flag = false;
+#     int len = n.length();
+#     //int num = stoi(n);
+#     for(int i = 0; i < len; i++){
+#         if(i + 6 < len){
+#             if((n[i] == n[i+1] == n[i+2] == n[i+3] == n[i+4] == n[i+5] == n[i+6])){    
+#                 cout << "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+#                 flag = true;
+#             }
+#         }
+#     }  
+#     if(flag) cout << "YES";
+#     else cout << "NO";
+# }
