e847bccb572fba7e2695ff7ecf3d98424e1a6b26
Pokloskaya <master7n7@gmail.com>
2023-02-10 18:56:08 -0500

    almost finished exercise

--- cp/Round_#849 (Div. 4)/Prepend&Append.cpp
+++ cp/Round_#849 (Div. 4)/Prepend&Append.cpp
@@ -2,25 +2,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int t;
-    cin >> t;
+int main()
+{
+    int testCase;
+    cin>>testCase;
+  
+	for(int z = 0; z < testCase; z++){
+        int len; cin >> len;
+        string n; cin >> n;
 
-    //cout << "no" << endl;
-    while(t--){
-        int n; cin >> n;
-        int bin; cin >> bin;
-
-        if(n == 1);
+        if(len == 1){
             cout << n << endl;
             continue;
+        }
 
-        if(n == 2);
-            if(n[0] == n[1])
+        if(len == 2){
+            if(n[0] == n[1]){
+                cout << 2 << endl;  //si los dos son iguales, no pudieron empezar en cero
+            }  
+            else{
+                cout << 0 << endl; 
+            } 
             continue;
+        }
 
+        if(len == 3){
+            if(n[0] == n[2]) cout << n << endl;
+            else cout << 1 << endl;
+            continue;
+        }
         
-        
+        int mit = len/2;  ///si es impar es imposible que haya empezado en cero!
+        int contador = 0; //vamos a ver los extremos e ir ocntando
+
+
+        int j = len-1;
+        for(int i = 0; i < mit; i++)
+        {
+            if(n[i] == n[j]){
+                // cout << n[i] << " " << n[j] << "<-- Son iguales!" << endl; 
+                // cout << i << " " << j << "<-- Son posicion!" << endl; 
+                contador = (j-i)+1;
+                cout << contador << endl; 
+                continue;
+            }
+            j--;
+        }      
     }
     return 0;
 }
--- cp/fubol.py
+++ cp/fubol.py
@@ -6,10 +6,8 @@ for i in range(len(n)):
         if(n[i] == n[i+1] == n[i+2] == n[i+3] == n[i+4] == n[i+5] == n[i+6]):
             flag = True
 
-if(flag):
-    print("YES")
-else:
-    print("NO")
+if(flag): print("YES")
+else: print("NO")
 
 
 # //https://codeforces.com/problemset/problem/96/A
