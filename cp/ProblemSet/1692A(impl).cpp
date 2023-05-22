6750bcfd21db85ba3b2622c9c954a0c696caf0b6
Pokloskaya <master7n7@gmail.com>
2023-05-21 23:12:38 -0500

    Added impl exercise

--- cp/ProblemSet/1692A(impl).cpp
+++ cp/ProblemSet/1692A(impl).cpp
@@ -1,10 +1,11 @@
-//
+//https://codeforces.com/contest/1692/problem/A
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 #define       forn(i,n)              for(int i=0;i<n;i++)
 #define          all(v)              v.begin(), v.end()
 #define         rall(v)              v.rbegin(),v.rend()
+ 
 #define            pb                push_back
 #define          sz(a)               (int)a.size()
 //cout << "k2: " << k2 << endl;
@@ -12,17 +13,24 @@ using ll = long long;
 //vector<int> myVector; <------- vectores
 //cout << "debuggear: " << variable << endl;
 
-string function(string s){
+int solve(vector<int> myVector){
+    int result = 0;
+    for(int i = 1; i < 4; i++){
+        if(myVector[i] > myVector[0]) result++;
+    }
 
-    for(int i = 0; i < VARIABLE; i++){}
-
-    return VARIABLE;
+    return result;
 }
 
 int main(){
     int t; cin >> t;
     while(t--){
-        cout << function() << endl;
+        vector<int> myVector;
+        for(int i = 0; i < 4; i++){
+            int number; cin >> number;
+            myVector.push_back(number);
+        }
+        cout << solve(myVector) << endl;
     }
     
 }
